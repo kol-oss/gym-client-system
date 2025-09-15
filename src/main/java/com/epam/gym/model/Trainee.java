7b49a1b83ffc5aef539aa60420820f4a4e649a72
kol-oss <kolyakhozhajinov@gmail.com>
2025-09-16 00:06:59 +0300

    Add relations to domain model

--- src/main/java/com/epam/gym/model/Trainee.java
+++ src/main/java/com/epam/gym/model/Trainee.java
@@ -1,17 +1,26 @@
 package com.epam.gym.model;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
-@Builder
-public class Trainee extends User {
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Trainee extends User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
     private String address;
+
+    private Training training;
 }
