471b96a4a23b0edeaaa1f9af346d335b12bcce8a
kol-oss <kolyakhozhajinov@gmail.com>
2025-09-16 00:12:42 +0300

    Add app properties and update storage config

---
+++ src/main/java/com/epam/gym/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.epam.gym.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.epam.gym")
+public class AppConfig {
+}
